a144feb305a8676eed9a3fe2906128646b9ef99d
andreina marcano <andrevallegm@gmail.com>
2022-09-04 12:42:14 -0400

    desafio array

---
+++ index.js
@@ -0,0 +1,13 @@
+alert ("¡Hola! cuéntanos de donde eres 1.Argentina, 2.Chile, 3.Mexico, 4.Venezuela, 5.Brasil")
+let arrayPaises = ["Argentina", "Chile", "Mexico", "Venezuela", "Brasil"];
+console.log(arrayPaises);
+prompt("O si, eres de Colombia o Ecuador presione si")
+arrayPaises.push("Colombia", "Ecuador");
+console.log(arrayPaises);
+
+
+
+
+
+   
+
